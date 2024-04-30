b319ada28fb6c9d8cd9005186dc8d6eb7a390fe1
mindexpert7546 <Kundan.Kumar@iinterchange.net>
2024-04-30 19:12:20 +0530

    include path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PromoiseComponent } from './promoise/promoise.component';
+import { ObservableComponent } from './observable/observable.component';
 
 const routes: Routes = [
   {path :'promoise', component:PromoiseComponent},
-  {path :'**', component:PromoiseComponent}
+  {path : 'observable', component:ObservableComponent},
+  {path :'**', component:PromoiseComponent},
+ 
 ];
 
 @NgModule({
